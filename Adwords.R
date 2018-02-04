46c8205c6450fd97a3ec7fbb3174e12f763a65c5
Anita Owens <anita.owens@hyperisland.se>
2018-02-04 09:29:36 +0100

    Delete Adwords.R

--- Adwords.R
+++
@@ -1,9 +0,0 @@
-mydata <- read.table("Adwords.csv", header=TRUE, 
-                     sep=",",row.names='id')
-
-str(Adwords)
-
-hist(Adwords...Sheet1$X1.11.)
-# Take the log-transform of the amount, and plot
-Adwords$V25 = log(new_data$amount)
-hist(new_data$amount)
